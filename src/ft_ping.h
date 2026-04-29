f5e1eba5dc27cce47206dcb2f74a26db3accf950
Ocyn <niko.ocyn@gmail.com>
2026-04-29 17:09:34 +0200

    feat(api): define ICMP header structure and checksum evaluation

--- src/ft_ping.h
+++ src/ft_ping.h
@@ -6,12 +6,37 @@
 # include <unistd.h>
 # include <string.h>
 # include <netinet/ip_icmp.h>
+# include <sys/time.h>
 
-struct icmphdr;
+// struct icmphdr;
+typedef struct	s_icmp_header
+{
+	uint8_t type;
+	uint8_t code;
+	uint16_t checksum;
+	union
+	{
+		struct
+		{
+		uint16_t	id;
+		uint16_t	sequence;
+		} echo;	
+		uint32_t	gateway;
+		struct
+		{
+		uint16_t	__glibc_reserved;
+		uint16_t	mtu;
+		} frag;
+	};
+}	t_icmp_header;
+
+struct timeval;
 
 void lorem(char *arg); // A retirer avant rendu final
 int	process_input(int ac, char **av);
 int init_ping(char **av);
 int	return_error(char *log);
+
+unsigned long	eval_checksum(t_icmp_header header);
 
 #endif
